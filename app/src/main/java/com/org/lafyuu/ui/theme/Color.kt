98772ce237c190a052e7483017939a9206da51b9
marv-javv <marvellous.ayoola@oceangrsmith.com>
2024-10-22 09:33:55 +0100

    Added Lafyuu colors, fonts and assets

--- app/src/main/java/com/org/lafyuu/ui/theme/Color.kt
+++ app/src/main/java/com/org/lafyuu/ui/theme/Color.kt
@@ -2,10 +2,12 @@ package com.org.lafyuu.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val blue = Color(0xFF40BFFF)
+val red = Color(0xFFFB7181)
+val yellow = Color(0xFFFFC833)
+val green = Color(0xFF53D1B6)
+val purple = Color(0xFF5C61F4)
+val dark = Color(0xFF223263)
+val grey = Color(0xFF9098b1)
+val light = Color(0xFFebf0ff)
+val white = Color(0xFFFFFFFF)
--- app/src/main/java/com/org/lafyuu/ui/theme/Type.kt
+++ app/src/main/java/com/org/lafyuu/ui/theme/Type.kt
@@ -1,34 +1,105 @@
 package com.org.lafyuu.ui.theme
 
-import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.org.lafyuu.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+//Heading text styles
+
+val heading1 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 32.sp,
+    letterSpacing = 0.5.sp,
+)
+val heading2 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 24.sp,
+    letterSpacing = 0.5.sp,
+)
+val heading3 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 20.sp,
+    letterSpacing = 0.5.sp,
+)
+val heading4 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 16.sp,
+    letterSpacing = 0.5.sp,
+)
+val heading5 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 14.sp,
+    letterSpacing = 0.5.sp,
+)
+val heading6 = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 10.sp,
+    letterSpacing = 0.5.sp,
+)
+
+//Body text styles
+
+val largeTextBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 16.sp,
+    letterSpacing = 0.5.sp,
+)
+val largeTextRegular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+    fontSize = 16.sp,
+    letterSpacing = 0.5.sp,
+)
+val mediumTextBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 14.sp,
+    letterSpacing = 0.5.sp,
+)
+val mediumTextRegular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+    fontSize = 14.sp,
+    letterSpacing = 0.5.sp,
+)
+val smallTextBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 12.sp,
+    letterSpacing = 0.5.sp,
+)
+val smallTextRegular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+    fontSize = 12.sp,
+    letterSpacing = 0.5.sp,
+)
+
+//Caption text styles
+
+val largeCaptionBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 12.sp,
+    letterSpacing = 0.5.sp
+)
+val largeCaptionRegular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+    fontSize = 12.sp,
+    letterSpacing = 0.5.sp
+)
+val smallCaptionBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 10.sp,
+    letterSpacing = 0.5.sp
+)
+val smallCaptionRegular = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_regular)),
+    fontSize = 10.sp,
+    letterSpacing = 0.5.sp,
+)
+
+//Link text styles
+
+val largeLinkBold = TextStyle(
+    fontFamily = FontFamily(Font(R.font.poppins_bold)),
+    fontSize = 12.sp,
+    letterSpacing = 0.5.sp,
+    color = blue
 )
